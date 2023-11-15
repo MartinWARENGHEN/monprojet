4fa315409d3333ab127ceec389d928e90d77859a
@MartinWARENGHEN <martinwarenghen.scolaire@gmail.com>
2023-11-15 15:13:50 +0100

    Mise à jour de tous les fichiers en version 2

--- chapitre_include.php
+++ chapitre_include.php
@@ -21,7 +21,10 @@
 </nav> <!-- fermeture navbar -->
 
 <div id="contenu_1">
-	Ici mon bloc 1
+div id="contenu_1">
+	Ici mon bloc principal du fichier php
+</div> <!-- fermeture contenu_1 -->
+
 </div> <!-- fermeture contenu_1 -->
 
 <div id="contenu_2">
